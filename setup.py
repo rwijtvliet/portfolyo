1798788adb706a12b41067bf9ac30911562ebc93
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-03-23 00:43:50 +0100

    added versioneer

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+import versioneer
 
 setup(
     name="portfolyo",
-    version="0.0.1",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="Ruud Wijtvliet",
     packages=["portfolio"],
     description="Analysing and manipulating timeseries related to power and gas offtake portfolios.",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[line.strip() for line in open("requirements.txt", "r")],
 )
