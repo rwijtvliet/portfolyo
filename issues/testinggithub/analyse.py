81df1d54f145b9e5522124d86ac0019bb7d7d3ba
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-10-27 17:24:17 +0200

    more analyses

--- issues/testinggithub/analyse.py
+++ issues/testinggithub/analyse.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 
+FILES = "39finalok", "39firstnok"
+FILES = "38", "39", "310"
+
 packages = defaultdict(dict)
-for v in 38, 39, 310:
-    print(f"\n\n{v}")
-    for line in open(f"{v}.txt").readlines():
+for file in FILES:
+    print(f"\n\n{file}")
+    for line in open(f"{file}.txt").readlines():
         if line.startswith("Requirement already satisfied: "):
             print(line)
             packvers = line[31:-1].split(" ")
@@ -14,20 +17,24 @@ for v in 38, 39, 310:
                 pack = pack[:found]
             if pack == "c":
                 raise Exception()
-            packages[pack][v] = ver
+            packages[pack][file] = ver
 
-    for line in open(f"{v}.txt").readlines():
+    for line in open(f"{file}.txt").readlines():
         if line.startswith("Successfully installed"):
             print(line)
             packvers = line[23:-1].split(" ")
             for packver in packvers:
                 splitpoint = -packver[::-1].find("-")
                 pack, ver = packver[: splitpoint - 1], packver[splitpoint:]
-                packages[pack][v] = ver
+                packages[pack][file] = ver
 
 
 # check version diffs
-suspicious = {}
+suspicious_missing = {}
+suspicious_diffrnt = {}
 for pack, versions in packages.items():
-    if len(versions) < 3 or len(set(versions.items())) < 3:
-        suspicious[pack] = versions
+    if len(versions) < len(FILES):
+        suspicious_missing[pack] = versions
+    if len(set(versions.values())) > 1:
+        suspicious_diffrnt[pack] = versions
+suspicious = {"missing": suspicious_missing, "different": suspicious_diffrnt}
