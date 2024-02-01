756473f46e71d82fb952feae0de6e3f42e2c4ce5
rwijtvliet <rwijtvliet@gmail.com>
2024-02-01 13:59:58 +0100

    additional tests

---
+++ tests/core/pfline/test_pfline_text.py
@@ -0,0 +1,13 @@
+"""Test if portfolio line can be printed."""
+
+import pytest
+import portfolyo as pf
+
+
+@pytest.mark.parametrize("levels", [1, 2, 3])
+@pytest.mark.parametrize("kind", pf.Kind)
+@pytest.mark.parametrize("flatten", [True, False])
+def test_pfline_print(levels: int, kind: pf.Kind, flatten: bool):
+    """Test if portfolio line can be printed."""
+    pfl = pf.dev.get_pfline(kind=kind, nlevels=levels)
+    pfl.print(flatten)
