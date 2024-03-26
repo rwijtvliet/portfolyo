3ea44a69e9a032f764ad063572a262e4da6a215f
Alina Voilova <alina.voilova@lichtblick.de>
2024-03-26 15:56:39 +0100

    tests for slice

--- tests/core/pfstate/test_pfstate_init.py
+++ tests/core/pfstate/test_pfstate_init.py
@@ -506,3 +506,22 @@ def test_pfstate_consistency_nosourcing():
         pd.Series(1.0, i_ref, "pint[dimensionless]"),
         check_names=False,
     )
+
+
+@pytest.mark.parametrize("inclusive", ["left", "both"])
+@pytest.mark.parametrize("freq", ["15T", "H"])
+def test_contain_whole_day(inclusive: str, freq: str):
+    """An index must contain full days.
+    For hourly-or-shorter values, this means that the start time of the first period () must equal the end time of the
+    last period (), which is not the case."""
+    index = pd.date_range(
+        "2020-01-01", "2020-02-01", freq=freq, tz="Europe/Berlin", inclusive=inclusive
+    )
+    if inclusive == "left":
+        # This should work without any error
+        pfs = dev.get_pfstate(index)
+        assert isinstance(pfs, PfState)
+    else:
+        # For "both" inclusive, it should raise an error
+        with pytest.raises(ValueError):
+            pfs = dev.get_pfstate(index)
--- tests/core/pfstate/test_slice_state.py
+++ tests/core/pfstate/test_slice_state.py
@@ -5,7 +5,24 @@ import pandas as pd
 from portfolyo import dev
 
 
-@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D", "15T"])
+def get_idx(
+    startdate: str,
+    starttime: str,
+    enddate: str,
+    tz: str,
+    freq: str,
+    inclusive: str,
+) -> pd.DatetimeIndex:
+    # Empty index.
+    if startdate is None:
+        return pd.DatetimeIndex([], freq=freq, tz=tz)
+    # Normal index.
+    ts_start = pd.Timestamp(f"{startdate} {starttime}", tz=tz)
+    ts_end = pd.Timestamp(f"{enddate} {starttime}", tz=tz)
+    return pd.date_range(ts_start, ts_end, freq=freq, inclusive=inclusive)
+
+
+@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D"])
 @pytest.mark.parametrize("slice_start", ["2021", "2022", "2022-01-02"])
 @pytest.mark.parametrize(
     "slice_end",
@@ -16,8 +33,20 @@ from portfolyo import dev
         ("2022-01-02", "2022-01-01"),
     ],
 )
-def test_slice_state(slice_start, slice_end, freq):
-    index = pd.date_range("2020", "2024", freq=freq)
+@pytest.mark.parametrize("tz", [None, "Europe/Berlin"])
+@pytest.mark.parametrize("inclusive", ["left", "both"])
+@pytest.mark.parametrize("sod", ["00:00", "06:00"])
+def test_slice_state(
+    slice_start: str,
+    slice_end: str,
+    freq: str,
+    sod: str,
+    inclusive: str,
+    tz: str,
+):
+    index = get_idx(
+        "2020", starttime=sod, enddate="2024", freq=freq, inclusive=inclusive, tz=tz
+    )
     pfs = dev.get_pfstate(index)
 
     pfs_to_concat = [pfs.slice[: slice_end[0]], pfs.slice[slice_start:]]
@@ -25,7 +54,7 @@ def test_slice_state(slice_start, slice_end, freq):
     assert pfs_to_concat == pfs_to_concat2
 
 
-@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D", "15T"])
+@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D"])
 @pytest.mark.parametrize(
     "slice_start",
     [
@@ -35,13 +64,24 @@ def test_slice_state(slice_start, slice_end, freq):
         "2022-01-02 14:00",
     ],
 )
-def test_state_slice_start(slice_start, freq):
-    index = pd.date_range("2020", "2024", freq=freq, inclusive="left")
+@pytest.mark.parametrize("tz", [None, "Europe/Berlin"])
+@pytest.mark.parametrize("inclusive", ["left", "both"])
+@pytest.mark.parametrize("sod", ["00:00", "06:00"])
+def test_state_slice_start(
+    slice_start: str,
+    freq: str,
+    sod: str,
+    inclusive: str,
+    tz: str,
+):
+    index = get_idx(
+        "2020", starttime=sod, enddate="2024", freq=freq, inclusive=inclusive, tz=tz
+    )
     pfs = dev.get_pfstate(index)
     assert pfs.slice[slice_start:] == pfs.loc[slice_start:]
 
 
-@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D", "15T"])
+@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D"])
 @pytest.mark.parametrize(
     "slice_end",
     [
@@ -52,22 +92,32 @@ def test_state_slice_start(slice_start, freq):
         ("2022-01-02", "2022-01-01"),
     ],
 )
-def test_state_slice_end(slice_end, freq):
-    index = pd.date_range("2020", "2024", freq=freq, inclusive="left")
+@pytest.mark.parametrize("tz", [None, "Europe/Berlin"])
+@pytest.mark.parametrize("inclusive", ["left", "both"])
+@pytest.mark.parametrize("sod", ["00:00", "06:00"])
+def test_state_slice_end(slice_end: str, freq: str, sod: str, inclusive: str, tz: str):
+    index = get_idx(
+        "2020", starttime=sod, enddate="2024", freq=freq, inclusive=inclusive, tz=tz
+    )
     pfs = dev.get_pfstate(index)
     assert pfs.slice[: slice_end[0]] == pfs.loc[: slice_end[1]]
 
 
-@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D", "15T"])
+@pytest.mark.parametrize("freq", ["MS", "AS", "QS", "D"])
 @pytest.mark.parametrize(
     "where",
     ["2022", "2022-03", "2022-04-21", "2022-05-23 14:34"],
 )
-def test_state_slice_whole(where: str, freq: str):
+@pytest.mark.parametrize("tz", [None, "Europe/Berlin"])
+@pytest.mark.parametrize("inclusive", ["left", "both"])
+@pytest.mark.parametrize("sod", ["00:00", "06:00"])
+def test_state_slice_whole(where: str, freq: str, sod: str, inclusive: str, tz: str):
     """Test that slicing splits the pfl in 2 non-overlapping pieces without gap
     (i.e., ensure that each original timestamp is in exactly one of the resulting pieces.)
     """
-    index = pd.date_range("2020", "2024", freq=freq, inclusive="left")
+    index = get_idx(
+        "2020", starttime=sod, enddate="2024", freq=freq, inclusive=inclusive, tz=tz
+    )
     pfs = dev.get_pfstate(index)
     left, right = pfs.slice[:where], pfs.slice[where:]
     # Test that each timestamp is present at least once.
