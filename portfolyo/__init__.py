47e921a266c99a635e664d9551e0e1b4e8c47213
rwijtvliet <rwijtvliet@gmail.com>
2024-04-16 11:43:22 +0200

    initial commit

--- portfolyo/__init__.py
+++ portfolyo/__init__.py
@@ -1,7 +1,6 @@
 """Package to analyse and manipulate timeseries related to power and gas offtake portfolios."""
 
-
-from . import _version, dev, tools
+from . import _version, dev, testing, tools
 from .core import extendpandas  # extend functionalty of pandas
 from .core import suppresswarnings
 from .core.mixins.plot import plot_pfstates
@@ -9,11 +8,13 @@ from .core.pfline import Kind, PfLine, Structure, create
 from .core.pfstate import PfState
 from .prices.hedge import hedge
 from .prices.utils import is_peak_hour
+from .tools.changefreq import averagable as asfreq_avg
+from .tools.changefreq import summable as asfreq_sum
 from .tools.changeyear import characterize_index, map_frame_to_year
 from .tools.freq import FREQUENCIES
 from .tools.standardize import frame as standardize
 from .tools.tzone import force_agnostic, force_aware
-from .tools.unit import Q_, ureg, Unit
+from .tools.unit import Q_, Unit, ureg
 from .tools.wavg import general as wavg
 
 VOLUME = Kind.VOLUME
@@ -25,4 +26,4 @@ extendpandas.apply()
 suppresswarnings.apply()
 
 __version__ = _version.get_versions()["version"]
-__all__ = ["tools", "dev", "PfLine", "PfState"]
+__all__ = ["tools", "dev", "testing", "PfLine", "PfState"]
