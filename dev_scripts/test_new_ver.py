1ddfff1310256a9f5d9090fb6dd0973135b2f2fc
rwijtvliet <rwijtvliet@gmail.com>
2024-07-16 18:46:37 +0200

    Update test_new_ver.py
    commented out problematic script

--- dev_scripts/test_new_ver.py
+++ dev_scripts/test_new_ver.py
@@ -1,8 +1,8 @@
-from matplotlib import pyplot as plt
-import pandas as pd
-from portfolyo import Kind, dev
+# from matplotlib import pyplot as plt
+# import pandas as pd
+# from portfolyo import Kind, dev
 
-index = pd.date_range("2020-01-01", "2021-01-01", freq="MS", tz=None)
-pfl = dev.get_pfline(index, nlevels=2, childcount=1, kind=Kind.VOLUME)
-pfl.plot(children=True)
-plt.show()
+# index = pd.date_range("2020-01-01", "2021-01-01", freq="MS", tz=None)
+# pfl = dev.get_pfline(index, nlevels=2, childcount=1, kind=Kind.VOLUME)
+# pfl.plot(children=True)
+# plt.show()
