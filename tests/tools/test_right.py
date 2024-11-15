1b75959c2fad8f552f5c14bfd4e8cf2f15b91a13
rwijtvliet <rwijtvliet@gmail.com>
2024-11-15 20:54:34 +0100

    parametrized the test_right tests

--- tests/tools/test_right.py
+++ tests/tools/test_right.py
@@ -1,137 +1,242 @@
 import pandas as pd
 import pytest
-
+import datetime as dt
 from portfolyo import testing, tools
 
-# Here, only test that right-bound side is correctly calculated for VALID indices and
-# timestamps. DO NOT check if error is raised for INvalid indices.
 
-TESTCASES = [  # ts, freq, expected_ts_right
-    # First day of X and start of day.
-    ("2020-01-01", "15min", "2020-01-01 00:15"),
-    ("2020-01-01", "h", "2020-01-01 01:00"),
-    ("2020-01-01", "D", "2020-01-02"),
-    ("2020-01-01", "MS", "2020-02-01"),
-    ("2020-01-01", "QS", "2020-04-01"),
-    ("2020-01-01", "YS", "2021-01-01"),
-    # First day of X but not start of day.
-    ("2020-01-01 06:00", "15min", "2020-01-01 06:15"),
-    ("2020-01-01 06:00", "h", "2020-01-01 07:00"),
-    ("2020-01-01 06:00", "D", "2020-01-02 06:00"),
-    ("2020-01-01 06:00", "MS", "2020-02-01 06:00"),
-    ("2020-01-01 06:00", "QS", "2020-04-01 06:00"),
-    ("2020-01-01 06:00", "YS", "2021-01-01 06:00"),
-    # Not first day of X but start of day.
-    ("2020-04-01", "15min", "2020-04-01 00:15"),
-    ("2020-04-01", "h", "2020-04-01 01:00"),
-    ("2020-04-01", "D", "2020-04-02"),
-    ("2020-04-01", "MS", "2020-05-01"),
-    ("2020-04-01", "QS", "2020-07-01"),
-    ("2020-04-21", "15min", "2020-04-21 00:15"),
-    ("2020-04-21", "h", "2020-04-21 01:00"),
-    ("2020-04-21", "D", "2020-04-22"),
-    # Not first day of X and not start of day.
-    ("2020-04-01 06:00", "15min", "2020-04-01 06:15"),
-    ("2020-04-01 06:00", "h", "2020-04-01 07:00"),
-    ("2020-04-01 06:00", "D", "2020-04-02 06:00"),
-    ("2020-04-01 06:00", "MS", "2020-05-01 06:00"),
-    ("2020-04-01 06:00", "QS", "2020-07-01 06:00"),
-    ("2020-04-21 06:00", "15min", "2020-04-21 06:15"),
-    ("2020-04-21 06:00", "h", "2020-04-21 07:00"),
-    ("2020-04-21 06:00", "D", "2020-04-22 06:00"),
-]
-
-TESTCASES_DST = [  # ts, freq, expected_ts_right, periods
-    # Start of DST.
-    ("2020-03-29 01:00", "15min", "2020-03-29 01:15", 92 + 9 * 96),
-    ("2020-03-29 01:00", "h", "2020-03-29 03:00", 23 + 9 * 24),
-    ("2020-03-29 00:00", "D", "2020-03-30 00:00", None),
-    ("2020-03-29 01:00", "D", "2020-03-30 01:00", None),
-    ("2020-03-29 03:00", "D", "2020-03-30 03:00", None),
-    ("2020-03-29 06:00", "D", "2020-03-30 06:00", None),
-    ("2020-03-01 00:00", "MS", "2020-04-01 00:00", None),
-    ("2020-03-01 06:00", "MS", "2020-04-01 06:00", None),
-    ("2020-01-01 00:00", "QS", "2020-04-01 00:00", None),
-    ("2020-01-01 06:00", "QS", "2020-04-01 06:00", None),
-    # End of DST.
-    ("2020-10-25 01:00", "15min", "2020-10-25 01:15+0200", 100 + 9 * 96),
-    ("2020-10-25 02:00+0200", "15min", "2020-10-25 02:15+0200", 100 + 9 * 96),
-    ("2020-10-25 02:00+0100", "15min", "2020-10-25 02:15+0100", 10 * 96),
-    ("2020-10-25 03:00", "15min", "2020-10-25 03:15", 10 * 96),
-    ("2020-10-25 01:00", "h", "2020-10-25 02:00+0200", 25 + 9 * 24),
-    ("2020-10-25 02:00+0200", "h", "2020-10-25 02:00+0100", 25 + 9 * 24),
-    ("2020-10-25 02:00+0100", "h", "2020-10-25 03:00", 10 * 24),
-    ("2020-10-25 03:00", "h", "2020-10-25 04:00", 10 * 24),
-    ("2020-10-25 00:00", "D", "2020-10-26 00:00", None),
-    ("2020-10-25 01:00", "D", "2020-10-26 01:00", None),
-    ("2020-10-25 03:00", "D", "2020-10-26 03:00", None),
-    ("2020-10-25 06:00", "D", "2020-10-26 06:00", None),
-    ("2020-10-01 00:00", "MS", "2020-11-01 00:00", None),
-    ("2020-10-01 06:00", "MS", "2020-11-01 06:00", None),
-    ("2020-10-01 00:00", "QS", "2021-01-01 00:00", None),
-    ("2020-10-01 06:00", "QS", "2021-01-01 06:00", None),
-]
-
-TESTCASES_15min_STAMPONLY = [  # ts, expected_ts_right
-    # Start of DST.
-    ("2020-03-29 00:45", "2020-03-29 01:00"),
-    ("2020-03-29 01:45", "2020-03-29 03:00"),
-    ("2020-03-29 03:45", "2020-03-29 04:00"),
-    # End of DST.
-    ("2020-10-25 01:45", "2020-10-25 02:00+0200"),
-    ("2020-10-25 02:45+0200", "2020-10-25 02:00+0100"),
-    ("2020-10-25 02:45+0100", "2020-10-25 03:00+0100"),
-    ("2020-10-25 03:45", "2020-10-25 04:00"),
-]
+@pytest.fixture(params=[None, "Europe/Berlin", "Asia/Kolkata"])
+def tz(request) -> str | None:
+    return request.param
 
 
-@pytest.mark.parametrize("tz", [None, "Europe/Berlin", "Asia/Kolkata"])
-@pytest.mark.parametrize(("ts", "freq", "expected_ts_right"), TESTCASES)
-def test_right_index(ts: str, freq: str, tz: str, expected_ts_right: str):
-    """Test if right timestamp is correctly calculated for index without dst-transition."""
-    do_test_index(ts, freq, tz, expected_ts_right, 96)  # always integer # of days
+@pytest.fixture(params=["2020-01-01", "2020-04-01"])
+def startdate(request) -> dt.date:
+    return dt.date.fromisoformat(request.param)
 
 
-@pytest.mark.parametrize(("ts", "freq", "expected_ts_right", "periods"), TESTCASES_DST)
-def test_right_index_dst(ts: str, freq: str, expected_ts_right: str, periods: int):
-    """Test if right timestamp is correctly calculated for index with dst-transition."""
-    periods = periods or 10
-    do_test_index(ts, freq, "Europe/Berlin", expected_ts_right, periods)
+@pytest.fixture(params=["00:00", "06:00"])
+def startofday(request) -> str:
+    return request.param
 
 
-@pytest.mark.parametrize("tz", [None, "Europe/Berlin", "Asia/Kolkata"])
-@pytest.mark.parametrize(("ts", "freq", "expected_ts_right"), TESTCASES)
-def test_right_stamp(ts: str, freq: str, tz: str, expected_ts_right: str):
-    """Test if right timestamp is correctly calculated for timestamp without dst-transition."""
-    do_test_stamp(ts, freq, tz, expected_ts_right)
+@pytest.fixture(params=["15min", "h", "D", "MS", "QS", "YS"])
+def freq(request) -> str:
+    return request.param
 
 
-@pytest.mark.parametrize(("ts", "freq", "expected_ts_right", "periods"), TESTCASES_DST)
-def test_right_stamp_dst(ts: str, freq: str, expected_ts_right: str, periods: int):
-    """Test if right timestamp is correctly calculated for timestamp with dst-transition."""
-    do_test_stamp(ts, freq, "Europe/Berlin", expected_ts_right)
+@pytest.fixture
+def startts(startdate, startofday, tz) -> pd.Timestamp:
+    return pd.Timestamp(f"{startdate} {startofday}", tz=tz)
 
 
-@pytest.mark.parametrize(("ts", "expected_ts_right"), TESTCASES_15min_STAMPONLY)
-def test_right_stamp_15min(ts: str, expected_ts_right: str):
-    """Test if right timestamp is correctly calculated for timestamp at non-round hour."""
-    do_test_stamp(ts, "15min", "Europe/Berlin", expected_ts_right)
+@pytest.fixture
+def periods(freq) -> int:
+    # 10 days or 3 months, quarters, years
+    return {"15min": 960, "h": 240, "D": 10}.get(freq, 3)
 
 
-def do_test_index(ts, freq, tz, expected_ts_right, periods):
-    # Must specify timezone in timestamp (instead of index) to avoid confusion at DST-changeover.
-    i = pd.date_range(pd.Timestamp(ts, tz=tz), freq=freq, periods=periods)
-    result = tools.right.index(i)
-    expected = pd.date_range(
-        pd.Timestamp(expected_ts_right, tz=tz), freq=freq, periods=periods, name="right"
-    )
+@pytest.fixture
+def index(startts, freq, periods) -> pd.DatetimeIndex:
+    return pd.date_range(startts, freq=freq, periods=periods)
+
+
+@pytest.fixture
+def startts_right(startdate, startofday, freq, tz) -> pd.Timestamp:
+    time = dt.time.fromisoformat(startofday)
+    if freq == "15min":
+        time = time.replace(minute=time.minute + 15)
+    elif freq == "h":
+        time = time.replace(hour=time.hour + 1)
+    elif freq == "D":
+        startdate = startdate.replace(day=startdate.day + 1)
+    elif freq == "MS":
+        startdate = startdate.replace(month=startdate.month + 1)
+    elif freq == "QS":
+        startdate = startdate.replace(month=startdate.month + 3)
+    elif freq == "YS":
+        startdate = startdate.replace(year=startdate.year + 1)
+    return pd.Timestamp(f"{startdate} {time}", tz=tz)
+
+
+@pytest.fixture
+def index_right(startts_right, freq, periods):
+    return pd.date_range(startts_right, freq=freq, periods=periods, name="right")
+
+
+def test_right_index(index, index_right):
+    """Test if right index is correctly calculated for index without dst-transition."""
+    result = tools.right.index(index)
+    expected = index_right
     testing.assert_index_equal(result, expected)
-    if freq != "15min":
-        testing.assert_index_standardized(result)
 
 
-def do_test_stamp(ts, freq, tz, expected_ts_right):
-    ts = pd.Timestamp(ts, tz=tz)
-    result = tools.right.stamp(ts, freq)
-    expected = pd.Timestamp(expected_ts_right, tz=tz)
+def test_right_stamp(startts, startts_right, freq):
+    """Test if right timestamp is correctly calculated for timestamp without dst-transition."""
+    result = tools.right.stamp(startts, freq)
+    expected = startts_right
+    assert result == expected
+
+
+# DST
+
+# . General
+
+
+@pytest.fixture(params=["15min", "h", "D"])
+def freq_dst(request):
+    return request.param
+
+
+# . Start of dst (winter -> summer).
+
+
+@pytest.fixture(params=["2020-03-29"])
+def startdate_dst1(request) -> dt.date:
+    return dt.date.fromisoformat(request.param)
+
+
+@pytest.fixture(params=["00:00", "01:00", "03:00"])
+def startofday_dst1(request) -> str:
+    return request.param
+
+
+@pytest.fixture
+def startts_dst1(startdate_dst1, startofday_dst1) -> pd.Timestamp:
+    return pd.Timestamp(f"{startdate_dst1} {startofday_dst1}", tz="Europe/Berlin")
+
+
+@pytest.fixture
+def periods_dst1(startofday_dst1, freq_dst) -> int:
+    # 10 days
+    if freq_dst == "15min":
+        day1 = 92 if startofday_dst1 in ["00:00", "01:00"] else 96
+        return day1 + 9 * 96
+    elif freq_dst == "h":
+        day1 = 23 if startofday_dst1 in ["00:00", "01:00"] else 24
+        return day1 + 9 * 24
+    elif freq_dst == "D":
+        return 10
+    raise ValueError()
+
+
+@pytest.fixture
+def index_dst1(startts_dst1, freq_dst, periods_dst1) -> pd.DatetimeIndex:
+    return pd.date_range(startts_dst1, freq=freq_dst, periods=periods_dst1)
+
+
+@pytest.fixture
+def startts_right_dst1(startdate_dst1, startofday_dst1, freq_dst) -> pd.Timestamp:
+    time = dt.time.fromisoformat(startofday_dst1)
+    if freq_dst == "15min":
+        time = time.replace(minute=time.minute + 15)
+    elif freq_dst == "h":
+        delta_hours = 2 if startofday_dst1 == "01:00" else 1
+        time = time.replace(hour=time.hour + delta_hours)
+    elif freq_dst == "D":
+        startdate_dst1 = startdate_dst1.replace(day=startdate_dst1.day + 1)
+    return pd.Timestamp(f"{startdate_dst1} {time}", tz="Europe/Berlin")
+
+
+@pytest.fixture
+def index_right_dst1(startts_right_dst1, freq_dst, periods_dst1):
+    return pd.date_range(
+        startts_right_dst1, freq=freq_dst, periods=periods_dst1, name="right"
+    )
+
+
+def test_right_index_dst1(index_dst1, index_right_dst1):
+    """Test if right index is correctly calculated for index with winter->summer dst-transition."""
+    result = tools.right.index(index_dst1)
+    expected = index_right_dst1
+    testing.assert_index_equal(result, expected)
+
+
+def test_right_stamp_dst1(startts_dst1, startts_right_dst1, freq_dst):
+    """Test if right timestamp is correctly calculated for timestamp with winter->summer dst-transition."""
+    result = tools.right.stamp(startts_dst1, freq_dst)
+    expected = startts_right_dst1
+    assert result == expected
+
+
+# . End of dst (summer -> winter).
+
+
+@pytest.fixture(params=["2020-10-25"])
+def startdate_dst2(request) -> dt.date:
+    return dt.date.fromisoformat(request.param)
+
+
+@pytest.fixture(params=["00:00", "01:00", "02:00+0200", "02:00+0100", "03:00"])
+def startofday_dst2(request) -> str:
+    return request.param
+
+
+@pytest.fixture
+def startts_dst2(startdate_dst2, startofday_dst2) -> pd.Timestamp:
+    return pd.Timestamp(f"{startdate_dst2} {startofday_dst2}", tz="Europe/Berlin")
+
+
+@pytest.fixture
+def periods_dst2(startofday_dst2, freq_dst) -> int:
+    # 10 days
+    if freq_dst == "15min":
+        day1 = 100 if startofday_dst2 in ["00:00", "01:00", "02:00+0200"] else 96
+        return day1 + 9 * 96
+    elif freq_dst == "h":
+        day1 = 25 if startofday_dst2 in ["00:00", "01:00", "02:00+0200"] else 24
+        return day1 + 9 * 24
+    elif freq_dst == "D":
+        return 10
+    raise ValueError()
+
+
+@pytest.fixture
+def index_dst2(
+    startts_dst2, freq_dst, periods_dst2, startofday_dst2
+) -> pd.DatetimeIndex:
+    if startofday_dst2 in ["02:00+0200", "02:00+0100"] and freq_dst == "D":
+        pytest.skip(
+            "Pandas thinks this is ambiguous, even though we have '+0100' or '+0200' in the timestamp."
+        )
+    return pd.date_range(startts_dst2, freq=freq_dst, periods=periods_dst2)
+
+
+@pytest.fixture
+def startts_right_dst2(startdate_dst2, startofday_dst2, freq_dst) -> pd.Timestamp:
+    if freq_dst == "15min":
+        timestr = startofday_dst2.replace(":00", ":15")
+    elif freq_dst == "h":
+        timestr = {
+            "00:00": "01:00",
+            "01:00": "02:00+0200",
+            "02:00+0200": "02:00+0100",
+            "02:00+0100": "03:00",
+            "03:00": "04:00",
+        }[startofday_dst2]
+    elif freq_dst == "D":
+        if startofday_dst2 in ["02:00+0200", "02:00+0100"]:
+            pytest.skip("Unclear what correct outcome is in this situation.")
+        timestr = startofday_dst2
+        startdate_dst2 = startdate_dst2.replace(day=startdate_dst2.day + 1)
+    return pd.Timestamp(f"{startdate_dst2} {timestr}", tz="Europe/Berlin")
+
+
+@pytest.fixture
+def index_right_dst2(startts_right_dst2, freq_dst, periods_dst2):
+    return pd.date_range(
+        startts_right_dst2, freq=freq_dst, periods=periods_dst2, name="right"
+    )
+
+
+def test_right_index_dst2(index_dst2, index_right_dst2):
+    """Test if right index is correctly calculated for index with summer->winter dst-transition."""
+    result = tools.right.index(index_dst2)
+    expected = index_right_dst2
+    testing.assert_index_equal(result, expected)
+
+
+def test_right_stamp_dst2(startts_dst2, startts_right_dst2, freq_dst):
+    """Test if right timestamp is correctly calculated for index with summer->winter dst-transition."""
+    result = tools.right.stamp(startts_dst2, freq_dst)
+    expected = startts_right_dst2
     assert result == expected
