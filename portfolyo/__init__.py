c0d71ea0776206a0d8f2e0b82968624cb1b741e2
Alina Voilova <91426818+Pizza2Pizza@users.noreply.github.com>
2025-03-24 10:25:38 +0100

    version comparison on push

--- portfolyo/__init__.py
+++ portfolyo/__init__.py
@@ -20,9 +20,8 @@ from .tools.standardize import frame as standardize
 from .tools.tzone import force_agnostic, force_aware
 from .tools.unit import Q_, Unit, ureg
 from .tools.wavg import general as wavg
+from .__version__ import __version__
 
-import tomli
-from pathlib import Path
 
 VOLUME = Kind.VOLUME
 PRICE = Kind.PRICE
@@ -33,27 +32,4 @@ extendpandas.apply()
 suppresswarnings.apply()
 
 
-# def get_version():
-#     # Find the pyproject.toml file relative to this file
-#     pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
-
-#     # Open and read the pyproject.toml file using tomli
-#     with pyproject_path.open("rb") as f:
-#         pyproject_data = tomli.load(f)
-#         return pyproject_data["tool"]["poetry"]["version"]
-
-
-def get_version():
-    # Find the __version__.py file in the same directory as this script
-    version_file = Path(__file__).parent / "__version__.py"
-
-    # Read the file and extract the version number
-    version_globals = {}
-    with version_file.open("r", encoding="utf-8") as f:
-        exec(f.read(), version_globals)  # Execute the file to get __version__
-
-    return version_globals["__version__"]  # Return the extracted version
-
-
-__version__ = get_version()
 __all__ = ["tools", "dev", "PfLine", "PfState"]
